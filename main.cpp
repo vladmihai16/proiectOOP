cba74372187fbdc36392a0af71bd7397c830ce27
vladmihai16 <67113784+vladmihai16@users.noreply.github.com>
2023-11-13 16:48:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,17 @@ enum class EventType { Movie, Sports, Concert };
 enum class TicketType { Child, Student, Adult, Elder };
 enum class Discounts { Adult = 0, Child = 50, Student = 40, Elder = 30 };
 
-class Event{
+class Event {
 private:
     std::string name = "placeholder";
     std::string city = "placeholder";
     std::string venue = "placeholder";
     std::string date = "1/01/1970";
     std::string time = "00:00";
-    // Removed unused pointers
+    char* seatType = nullptr;
+    int lengthOfSeatType = 0;
+    char* takenSeatsNormal = nullptr;
+    char* takenSeatsVIP = nullptr;
     int noOfRows = 0;
     int noOfColumns = 0;
     int price = 0;
@@ -32,67 +35,84 @@ private:
     static const int MIN_PRICE = 1;
 
 public:
-    void setEventType(EventType type){
+    void setEventType(EventType eventType) {
         // Implementation needed
     }
 
-    void setCity(std::string newCity){
-        if (newCity.size() < Event::MIN_CITY_LENGTH){
-            throw std::exception("City name is too short!");
+    void setCity(const std::string& newCity) {
+        if (newCity.size() < Event::MIN_CITY_LENGTH) {
+            throw std::invalid_argument("City name is too short!");
         }
         this->city = newCity;
     }
 
-    void setName(std::string newName){
+    void setName(const std::string& newName) {
         if (newName.size() < Event::MIN_NAME_LENGTH) {
-            throw std::exception("Name is too short!");
+            throw std::invalid_argument("Name is too short!");
         }
         this->name = newName;
     }
 
-    void setDate(std::string newDate){
+    void setDate(const std::string& newDate) {
         if (newDate.size() != Event::MAX_DATE_SIZE) {
-            throw std::exception("Date format is not valid!");
+            throw std::invalid_argument("Date format is not valid!");
         }
-        // Implementation needed to update date
+        this->date = newDate;
     }
 
-    void setNoOfRows(int newNumber){
-        if (newNumber >= MIN_ROWS){
+    void setNoOfRows(int newNumber) {
+        if (newNumber >= MIN_ROWS) {
             this->noOfRows = newNumber;
-        }
-        else {
-            throw std::exception("Number of rows is too small!");
+        } else {
+            throw std::invalid_argument("Number of rows is too small!");
         }
     }
 
-    void setNoOfColumns(int newNumber){
-        if(newNumber >= MIN_COLUMNS){
+    void setNoOfColumns(int newNumber) {
+        if (newNumber >= MIN_COLUMNS) {
             this->noOfColumns = newNumber;
+        } else {
+            throw std::invalid_argument("Number of columns is too small!");
         }
-        else{
-            throw std::exception("not valid Number of Columns at the Venue!");
+    }
+
+    void setTakenSeatsNormal(int row, int column) {
+        if (row >= 1 && row <= noOfRows && column >= 1 && column <= noOfColumns) {
+            int seatIndex = (row - 1) * noOfColumns + column - 1;
+            takenSeatsNormal[seatIndex] = 'O'; //'O' indicates the seat is taken
+        } else {
+            throw std::out_of_range("Invalid seat coordinates!");
         }
     }
 
-    void settakenSeatsNormal(int newSeat){
-        // Implementation needed
+    void setTakenSeatsVIP(int row, int column) {
+        if (row >= 1 && row <= noOfRows && column >= 1 && column <= noOfColumns) {
+            int seatIndex = (row - 1) * noOfColumns + column - 1;
+            takenSeatsVIP[seatIndex] = 'O'; //idem setTakenSeatsNormal
+        } else {
+            throw std::out_of_range("Invalid seat coordinates!");
+        }
     }
 
-    void settakenSeatsVIP(int newSeat){
-        // Implementation needed
-    }
-
-    void setPrice(int newPrice){
-        if (newPrice < MIN_PRICE){
-            throw std::exception("Price is too small");
+    void setPrice(int newPrice) {
+        if (newPrice < MIN_PRICE) {
+            throw std::invalid_argument("Price is too small");
         }
         this->price = newPrice;
     }
 
-    // Getters go here
+    //Getters 
 };
 
 int main() {
-   return 0;
+    Event myEvent;
+    myEvent.setCity("New York");
+    myEvent.setNoOfRows(5);
+    myEvent.setNoOfColumns(10);
+    myEvent.setTakenSeatsNormal(3, 5);
+    myEvent.setTakenSeatsVIP(2, 8);
+
+    //... restul codului
+
+    return 0;
 }
