0182347565797405f2b0504747022547267f9131
Vlad Mihai <vladutmp@gmail.com>
2023-11-25 23:46:18 +0200

    add copy constructor to Area

--- area.h
+++ area.h
@@ -9,10 +9,38 @@ class Area{
 private:
 
 	std::string name;
-	char* rows;
-	short* firstSeats;
-	short* lastSeats;
+	short index;
+	char* rows = nullptr;
+	short* firstSeats = nullptr;
+	short* lastSeats = nullptr;
 	short noOfRows = 0;
+
+	static short noOfAreas;
+
+	void allocateRows(short n){
+		// allocates new arrays of n size
+		char* rowsCopy = (char*) malloc(sizeof (char) * n);
+		short* firstSeatsCopy = (short*) malloc(sizeof (short) * n);
+		short* lastSeatsCopy = (short*) malloc(sizeof (short) * n);
+
+		// copy data over
+		for(int i = 0; i < noOfRows; i++){
+			rowsCopy[i] = rows[i];
+			firstSeatsCopy[i] = firstSeats[i];
+			lastSeatsCopy[i] = lastSeats[i];
+		}
+
+		// frees the original arrays
+		free(rows); free(firstSeats); free(lastSeats);
+
+		// replaces the original arrays with the new ones
+		rows = rowsCopy;
+		firstSeats = firstSeatsCopy;
+		lastSeats = lastSeatsCopy;
+
+		// updates noOfRows
+		noOfRows = n;
+	}
 
 public:
 
@@ -28,34 +56,19 @@ public:
 	}
 
 	Area& addRow(char row, short firstSeat, short lastSeat){
-		//makes space for one extra row by copying the arrays over
-		char* rowsCopy = (char*) malloc(sizeof (char) * (noOfRows + 1));
-		short* firstSeatsCopy = (short*) malloc(sizeof (short) * (noOfRows + 1));
-		short* lastSeatsCopy = (short*) malloc(sizeof (short) * (noOfRows + 1));
 
-		for(int i = 0; i < noOfRows; i++){
-			rowsCopy[i] = rows[i];
-			firstSeatsCopy[i] = firstSeats[i];
-			lastSeatsCopy[i] = lastSeats[i];
-		}
-
-		// adds the values of the new rows
-		rowsCopy[noOfRows] = row;
-		firstSeatsCopy[noOfRows] = firstSeat;
-		lastSeatsCopy[noOfRows] = lastSeat;
-
-		// replaces the original arrays with the new ones
-		rows = rowsCopy;
-		firstSeats = firstSeatsCopy;
-		lastSeats = lastSeatsCopy;
-
-		// increments the sizes of the new arrays
-		noOfRows++;
+		allocateRows(noOfRows+1);
+		// adds the values on the last positions
+		rows[noOfRows-1] = row;
+		firstSeats[noOfRows-1] = firstSeat;
+		lastSeats[noOfRows-1] = lastSeat;
 
 		return *this;
 	}
 
 	friend std::ostream& operator<<(std::ostream& out, Area& a){
+
+		out << "area number " << a.index << ": " << a.name << std::endl;
 
 		for(int i = 0; i < a.noOfRows; i++){
 			out << a.rows[i] << " ";
@@ -64,10 +77,53 @@ public:
 			}
 			out << std::endl;
 		}
-
 		return out;
 	}
 
+	friend std::istream& operator>>(std::istream& in, Area& a){
+		
+		std::cout << "number of rows: "; in >> a.noOfRows;
 
+		for(int i = 0; i < a.noOfRows; i++){
+			std::cout << "enter row letter, first seat number and last seat number"; 
+			in >> a.rows[i] >> a.firstSeats[i] >> a.lastSeats[i];
+		}
+		return in;
+	}
 
+	/*friend std::string operator[](Area& a, char row){
+
+		std::string output 
+		for(int i = 0; i < a.noOfRows && row != a.rows[i]; i++);
+		if(i < a.noOfRows){
+			output += std::to_string(a.rows[i]) + " ";
+			for(int j = a.firstSeats[i]; j <= a.lastSeats[i]; j++){
+				output += std::to_string(j) + " ";
+			}
+		}
+		return output;
+	}*/
+	// std::string operator[](Area& a, char row) must be a non-static member function
+
+	// default constructor
+	Area(){
+		index = ++noOfAreas;
+	}
+
+	// copy constructor
+	Area(Area& a){
+		index = ++noOfAreas;
+		name = a.name;
+		rows = a.rows;
+		firstSeats = a.firstSeats;
+		lastSeats = a.lastSeats;
+		noOfRows = a.noOfRows;
+		allocateRows(noOfRows);
+	}
+
+	static short getNoOfAreas(){
+		return noOfAreas;
+	}
 };
+
+short Area::noOfAreas = 0;
--- tests/area.cpp
+++ tests/area.cpp
@@ -1,14 +1,18 @@
 #include "../area.h"
 
 int main(){
-	Area a1;
+	Area a1, a2;
 
 	a1.getName();
 	a1.setName("VIP");
 
 	a1.addRow('A', 1, 20).addRow('B', 5, 17);
-	std::cout << a1;
+	a2 = a1;
+	std::cout << a1 << a2;
 
+	std::cout << "there are " << Area::getNoOfAreas() << " areas in total" << std::endl;
+
+	// a1['B'];
 
 	return 0;
 }
